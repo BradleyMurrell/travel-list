7fc6bd801e2825493e78c8af3cc0ef0bbe954a82
BradleyMurrell <95315836+BradleyMurrell@users.noreply.github.com>
2023-07-20 22:29:14 +0000

    Add basic markup for each component

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,11 +1,34 @@
 export default function App() {
-  return <div>TEST</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-function Logo() {}
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
 
-function Form() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
 
-function PackingList() {}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
 
-function Stats() {}
+function Stats() {
+  return (
+    <footer>
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
